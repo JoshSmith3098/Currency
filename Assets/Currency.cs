d02172a43e9a10ef3fcb7f1b2195eab2988b0476
JoshSmith3098 <U1657131@unimail.hud.ac.uk>
2017-02-06 09:36:22 +0000

    Currency (Start)
    This is the Start of the Variable (Global) for the currency and the
    currency system

---
+++ Assets/Currency.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Currency : MonoBehaviour {
+        private static Currency instance = new Currency();
+
+        public static Currency Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public string[] GlobalArray { get; set; }
+    }
+
+    // to call this code ue the following piece of code
+    //var myGlobalArray = Currency.Instance.GlobalArray;
---
+++ Assets/Score.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+    public GUIText scoreText;
+    private int GearValues;
+    void Start()
+    {
+        var myGlobalArray = Currency.Instance.GlobalArray; ;
+        UpdateScore();
+
+    }
+    public void AddScore(int newScoreValue)
+    {
+        GearValues += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + GearValues;
+    }
+}
